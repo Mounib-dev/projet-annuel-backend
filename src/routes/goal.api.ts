d698650c967122ba7a57452c05b81fedc4b9b352
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-06-21 16:01:01 +0200

    Generate Reccomandation route

--- src/routes/goal.api.ts
+++ src/routes/goal.api.ts
@@ -7,14 +7,18 @@ import {
   deleteGoal,
 } from "../controllers/goal/goal";
 import authorize from "../middlewares/auth";
+import { generateFinancialRecommandation } from "../controllers/goal/recommandation";
 
 const router = express.Router();
 
 router.use(authorize);
+
 router.post("/", createGoal);
 router.get("/", getUserGoals);
 router.get("/:id", getGoalById);
 router.patch("/:id", updateGoal);
 router.delete("/:id", deleteGoal);
+
+router.post("/recommandation/:id", generateFinancialRecommandation);
 
 export default router;
