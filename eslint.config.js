96a6905340bb2e7e69457f914b4e00f572cd7505
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-07-30 08:23:29 +0200

    Add and configure ESLINT

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import tseslint from "typescript-eslint";
+
+export default tseslint.config([
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+
+      "no-console": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+]);
