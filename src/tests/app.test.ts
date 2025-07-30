06e9ff2acd1fee09b6273173a99c7c649a66b5a1
OUROUA M. Mounib <ourouamounib@gmail.com>
2025-07-30 09:58:40 +0200

    Make server connection shut down after tests are running so Jest don't block CI

--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -1,6 +1,10 @@
 import request from "supertest";
 import { app, server } from "../app";
 
+afterAll((done) => {
+  server.close(done);
+});
+
 describe("GET /api/v1/", () => {
   it("should return 200 OK with the correct message", async () => {
     const response = await request(app).get("/api/v1/");
